ce5bffb4e421d79b082c9addca2b4590be0122c0
Josiel Delgadillo <josieldelgadillo98@gmail.com>
2020-03-23 07:58:15 -0400

    Configuring HashRouter references

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,26 +3,26 @@ import "../App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 
-//https://www.codyreppert.com/
+///myapp/# is added to href path due to production routing. 
 
 function Navigation() {
   return (
     <Navbar collapseOnSelect expand="lg" className="color-nav" variant="light">
-      <Navbar.Brand href="/" className="title">
+      <Navbar.Brand href="/myapp/#" className="title">
         JD
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav>
-          <Nav.Link href="/projects">Projects</Nav.Link>
+          <Nav.Link href="/myapp/#/projects">Projects</Nav.Link>
 
-          <Nav.Link href="/experiences">Experiences</Nav.Link>
+          <Nav.Link href="/myapp/#/experiences">Experiences</Nav.Link>
         </Nav>
       </Navbar.Collapse>
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="ml-auto">
-          <Nav.Link href="/resume">Resume</Nav.Link>
-          <Nav.Link href="/blog">Blog</Nav.Link>
+          <Nav.Link href="/myapp/#/resume">Resume</Nav.Link>
+          <Nav.Link href="/myapp/#/blog">Blog</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
