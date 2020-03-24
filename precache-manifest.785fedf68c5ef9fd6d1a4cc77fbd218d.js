4d097a319fc0527083b6cba15e52fb748a971831
Josiel Delgadillo <josieldelgadillo98@gmail.com>
2020-03-24 13:02:22 -0400

    Updates

---
+++ precache-manifest.785fedf68c5ef9fd6d1a4cc77fbd218d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31c1d0b5d1882480827a59391b5ab179",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "86d4ff5010f7a0adc4b0",
+    "url": "/portfolio/static/css/2.83767612.chunk.css"
+  },
+  {
+    "revision": "d3cd0e721adba6553114",
+    "url": "/portfolio/static/css/main.671e36c8.chunk.css"
+  },
+  {
+    "revision": "86d4ff5010f7a0adc4b0",
+    "url": "/portfolio/static/js/2.6282e13f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.6282e13f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3cd0e721adba6553114",
+    "url": "/portfolio/static/js/main.ac9cf470.chunk.js"
+  },
+  {
+    "revision": "f65a897cbbbd6907bb8c",
+    "url": "/portfolio/static/js/runtime-main.c4b71a62.js"
+  },
+  {
+    "revision": "b9268c429370022c5bffb823f0b06f90",
+    "url": "/portfolio/static/media/3081672.b9268c42.jpg"
+  },
+  {
+    "revision": "a23fa16cc5e58f3fb3087b966babbd6a",
+    "url": "/portfolio/static/media/Result_Video.a23fa16c.gif"
+  },
+  {
+    "revision": "44fcbe9a8856aec4e140252be390f43a",
+    "url": "/portfolio/static/media/Resume.44fcbe9a.pdf"
+  },
+  {
+    "revision": "61ad53298ab40943d56db9d8812a63c5",
+    "url": "/portfolio/static/media/Structure.61ad5329.png"
+  },
+  {
+    "revision": "8329e86109ee71e14655073f6c694250",
+    "url": "/portfolio/static/media/alexaintegration.8329e861.png"
+  },
+  {
+    "revision": "1578a88e8f6890aba123bac7a5e1cded",
+    "url": "/portfolio/static/media/avt-lane-detecton.1578a88e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ff6d24484c2bf3255e261cda4a252c86.js"
+  "/portfolio/precache-manifest.785fedf68c5ef9fd6d1a4cc77fbd218d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
