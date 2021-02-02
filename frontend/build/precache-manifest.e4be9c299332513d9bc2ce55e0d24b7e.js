1b3974961395b70d787f049017f85c1a49f0fece
Josiel Delgadillo <josieldelgadillo98@gmail.com>
2021-02-02 03:39:48 -0500

    Resume update

---
+++ frontend/build/precache-manifest.e4be9c299332513d9bc2ce55e0d24b7e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c14534943b9d3f121302c0e227ce1dac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f53d90a31854cda0891",
+    "url": "/static/css/2.58bdea55.chunk.css"
+  },
+  {
+    "revision": "38de56929db6c46c71b2",
+    "url": "/static/css/main.04b48b6c.chunk.css"
+  },
+  {
+    "revision": "1f53d90a31854cda0891",
+    "url": "/static/js/2.3bfcd4fa.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.3bfcd4fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38de56929db6c46c71b2",
+    "url": "/static/js/main.686c2efb.chunk.js"
+  },
+  {
+    "revision": "9d56b0ea2705241215be",
+    "url": "/static/js/runtime-main.cf8c75f0.js"
+  },
+  {
+    "revision": "b9268c429370022c5bffb823f0b06f90",
+    "url": "/static/media/3081672.b9268c42.jpg"
+  },
+  {
+    "revision": "a23fa16cc5e58f3fb3087b966babbd6a",
+    "url": "/static/media/Result_Video.a23fa16c.gif"
+  },
+  {
+    "revision": "523ff982331a19f50b59ba04f0ed7164",
+    "url": "/static/media/Resume.523ff982.pdf"
+  },
+  {
+    "revision": "61ad53298ab40943d56db9d8812a63c5",
+    "url": "/static/media/Structure.61ad5329.png"
+  },
+  {
+    "revision": "8329e86109ee71e14655073f6c694250",
+    "url": "/static/media/alexaintegration.8329e861.png"
+  },
+  {
+    "revision": "1578a88e8f6890aba123bac7a5e1cded",
+    "url": "/static/media/avt-lane-detecton.1578a88e.png"
+  },
+  {
+    "revision": "92771f7026639714e7c29c76bc31d9ea",
+    "url": "/static/media/pfizerinternship.92771f70.jpg"
+  },
+  {
+    "revision": "ca71657c8d91d36f5379a64ff318b0ee",
+    "url": "/static/media/sky.ca71657c.jpg"
+  }
+]);
